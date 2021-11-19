95cf7768366710a18c78aafa4f54aa6e11f3e30a
Pablo Fracaro <70729831+Pablex05@users.noreply.github.com>
2021-11-19 01:41:47 -0300

    Update conexion punto a punto.bat

--- conexion punto a punto.bat
+++ conexion punto a punto.bat
@@ -3,15 +3,7 @@ echo off
 CLS
 echo ===========CONEXION PUNTO A PUNTO========
 ECHO  
-netsh wlan set hostednetwork mode=allow ssid=pablex key=pablo123
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
-netsh wlan start hostednetwork
+netsh wlan set hostednetwork mode=allow ssid=Mi_red key=123456789
 netsh wlan start hostednetwork
 netsh wlan start hostednetwork
 
