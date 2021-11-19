b68cd4055054d58d903aefce717b5a8925ea830f
Pablex05 <70729831+Pablex05@users.noreply.github.com>
2021-11-18 21:20:56 -0300

    Add files via upload

---
+++ conexion punto a punto.bat
@@ -0,0 +1,19 @@
+
+echo off
+CLS
+echo ===========CONEXION PUNTO A PUNTO========
+ECHO  
+netsh wlan set hostednetwork mode=allow ssid=pablex key=pablo123
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+netsh wlan start hostednetwork
+
+
+pause
